6407fa17600967e842b476c7935cfc3198ebaf5d
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-20 18:56:58 +0200

    Exercise 1 done

--- part1/printing/exercise_01/Program.cs
+++ part1/printing/exercise_01/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_01
         public static void Main(string[] args)
         {
             // Add your code here:
+            Console.WriteLine("Hello World!");
             
+
+
+
+
+
         }
     }
 }
+
+
+
+
