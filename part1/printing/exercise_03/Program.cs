745fa51f354773bd5e52f2aa00fc64fa03000025
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-01-22 12:20:52 +0200

    Excersise_03 done

--- part1/printing/exercise_03/Program.cs
+++ part1/printing/exercise_03/Program.cs
@@ -7,6 +7,10 @@ namespace exercise_03
         public static void Main(string[] args)
         {
             // Your code here:
+            Console.WriteLine("Once upon a time\n" + 
+            "I was falling in love\n" +
+            "Now I'm only falling apart");
+            
         }
     }
 }
