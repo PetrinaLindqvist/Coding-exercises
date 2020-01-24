ab6cc6a94af809a274d948c37723438d3d1a7c0d
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-01-24 11:01:22 +0200

    exercise_09 done

--- part1/printing/exercise_09/Program.cs
+++ part1/printing/exercise_09/Program.cs
@@ -7,7 +7,12 @@ namespace exercise_09
         public static void Main(string[] args)
         {
             // Write your code here:
-
+            Console.WriteLine("Hello, how are you?");
+            string userInput1 = Console.ReadLine();
+            Console.WriteLine("That's interesting, tell me more");
+            string userInput2 = Console.ReadLine();
+            Console.WriteLine("Thank you for sharing" + "!");
         }
+
     }
 }
