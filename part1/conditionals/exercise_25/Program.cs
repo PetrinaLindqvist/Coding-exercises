8d2982a925281b1632b56d6d4331e21095971728
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-25 11:54:59 +0200

    exercise_25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_25
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("Give a number:");
+      string userNbr = Console.ReadLine();
+      int nbr = Convert.ToInt32(userNbr);
+      if (nbr == 1984)
+      {
+        Console.WriteLine("Orwell");
+      }
+   
     }
   }
 }
