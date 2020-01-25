7c07e758380a8117359eb56bb8e52e9846fb7d73
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-25 08:27:49 +0200

    Exercise_19 done

--- part1/calculations/exercise_17/Program.cs
+++ part1/calculations/exercise_17/Program.cs
@@ -11,12 +11,14 @@ namespace exercise_17
       Console.WriteLine("Give the first number!");
       string numBer1 = Console.ReadLine();
       int first = Convert.ToInt32(numBer1);
+
+
       Console.WriteLine("Give the second number!");
       string numBer2 = Console.ReadLine();
       int second = Convert.ToInt32(numBer2);
       Console.WriteLine("The sum is " + (first + second));
    
-   
+      Console.WriteLine(first + second = third)
     }
   }
 }
