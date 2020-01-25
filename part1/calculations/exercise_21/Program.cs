83d1e31ea218bda446f3364b6affcfb77a0009d8
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-25 09:24:43 +0200

    exercise_21 done

--- part1/calculations/exercise_21/Program.cs
+++ part1/calculations/exercise_21/Program.cs
@@ -8,7 +8,19 @@ namespace exercise_21
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string numBer1 = Console.ReadLine();
+      int first = Convert.ToInt32(numBer1);
+    
+      Console.WriteLine("Give the second number!");
+      string numBer2 = Console.ReadLine();
+      int second = Convert.ToInt32(numBer2);
+    
+      double average = (first + second) / 2.0;
+      Console.WriteLine("The average is " + average);
 
+  
+  
     }
   }
 }
