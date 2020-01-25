ebeb0f599baed5ec14a048c6088842e4ea35dbba
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-25 09:02:14 +0200

    Exercise_20 done

--- part1/calculations/exercise_20/Program.cs
+++ part1/calculations/exercise_20/Program.cs
@@ -8,7 +8,22 @@ namespace exercise_20
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string numBer1 = Console.ReadLine();
+      int first = Convert.ToInt32(numBer1);
+    
+      Console.WriteLine("Give the second number!");
+      string numBer2 = Console.ReadLine();
+      int second = Convert.ToInt32(numBer2);
+    
+      Console.WriteLine(first + " * " + second + " = " + (first*second));
+    
+    
+    
 
+    
+    
+    
     }
   }
 }
