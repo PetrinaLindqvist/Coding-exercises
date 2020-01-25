9980ead01dbc431e31082e7bc1bd1bbcfb459f28
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-25 13:01:15 +0200

    exercise_28 done

--- part1/conditionals/exercise_28/Program.cs
+++ part1/conditionals/exercise_28/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_28
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      Console.WriteLine("How old are you?");
+      string userNbr = Console.ReadLine();
+      int nbr = Convert.ToInt32(userNbr);
+      if (nbr >= 18 )
+      {
+        Console.WriteLine("You're an adult!");
+      }
+      else
+      {
+        Console.WriteLine("You're under age!");
+      }
     }
   }
 }
