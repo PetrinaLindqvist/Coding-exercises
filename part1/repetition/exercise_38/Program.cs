54c14b0e68b6960e5faf05049775614591b12d0d
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-26 16:20:12 +0200

    exercise_38 done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -5,9 +5,26 @@ namespace exercise_38
   class Program
   {
     public static void Main(string[] args)
+    
     {
       // Write your code here:
-
+      int countNeg = 0;
+      while (true)
+    {
+      Console.WriteLine("Give a number:"); 
+      int intValue = Convert.ToInt32(Console.ReadLine());
+      
+      if (intValue == 0)
+      {
+      break;      
+      }
+      if (intValue < 0)
+      {
+        countNeg ++;
+      }
     }
+    Console.WriteLine("Total amount of negative numbers: " + countNeg);
+  }
+    
   }
 }
