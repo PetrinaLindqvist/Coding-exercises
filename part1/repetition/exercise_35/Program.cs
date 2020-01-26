2db35d47e6453ab50c7ffe748f4555afa5eec86f
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-26 13:42:46 +0200

    exercise_35 done

--- part1/repetition/exercise_35/Program.cs
+++ part1/repetition/exercise_35/Program.cs
@@ -7,7 +7,18 @@ namespace exercise_35
     public static void Main(string[] args)
     {
       // Write your code here:
-
+      while (true)
+    {
+      Console.WriteLine("Give a number:");
+      String input = Console.ReadLine();
+      int intValue = Convert.ToInt32(input);
+      
+      if (input == "42")
+      {
+      break;
+      }
+    
+      }
     }
   }
 }
