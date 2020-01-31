4e3e677f1ff678e5585e04c3523a652b799d03ce
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-31 13:51:45 +0200

    exercise_49 done

--- part2/methods/exercise_49/Program.cs
+++ part2/methods/exercise_49/Program.cs
@@ -7,12 +7,13 @@ namespace exercise_49
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      PrintPhrase();
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -6,9 +6,18 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
-
+      
       // Write your code here:
- 
+      Console.WriteLine("Give numbers:");
+      while (true) 
+      {
+        int number = Convert.ToInt32(Console.ReadLine());
+        sum = sum + number;
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number == -1) break;
+      }
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + sum);
     }
   }
 }
