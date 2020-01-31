393cdb3f3d120600378f62b02e30fdfdcd26a7ca
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-01-31 17:47:00 +0200

    exercise_48 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,12 +7,20 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      Console.WriteLine("How many times?");
+      int times = Convert.ToInt32(Console.ReadLine());
+      int i = 0;
     }
-
+    while (i < times);
+    {
+      
+      PrintPhrase();
+      i++;
+    }
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -9,15 +9,39 @@ namespace exercise_48
       
       // Write your code here:
       Console.WriteLine("Give numbers:");
+      int summa = 0;
+      int validnumbers = 0;
+      int even = 0;
+      int odd = 0;
+      
       while (true) 
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number == -1) 
       {
-        int number = Convert.ToInt32(Console.ReadLine());
-        sum = sum + number;
-        int number = Convert.ToInt32(Console.ReadLine());
-        if (number == -1) break;
+        break;
       }
+       summa = summa + number;
+       validnumbers++;
+       if (number % 2 == 0)
+      {
+        even++;
+      }
+       else
+      {
+        odd++; 
+      }
+      
+      
+    }
+
+    double average = (double)summa / validnumbers;
       Console.WriteLine("Thx! Bye!");
-      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Sum: " + summa);
+      Console.WriteLine("Numbers: " + validnumbers);
+      Console.WriteLine("Average: " + average);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
     }
   }
 }
