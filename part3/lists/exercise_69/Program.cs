6acc30b6bc6f893b1f5a68e450741adfe9277434
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-02-05 14:39:49 +0200

    exaercise_69 done

--- part3/lists/exercise_69/Program.cs
+++ part3/lists/exercise_69/Program.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 
 namespace exercise_69
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
-        }
-        list.Add(input);
-      }
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            Console.WriteLine("From where?");
+            int from = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Where to?");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+
+            foreach (int number in list)
+            {
+            
+
+            if (number >= from && number <= end)
+            
+                Console.WriteLine(number);
+            }
+        }
     }
-  }
 }
