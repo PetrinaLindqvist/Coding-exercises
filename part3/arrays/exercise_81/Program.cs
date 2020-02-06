c2aea6ecf36774dbdb07f428d1f461eeb36ab86e
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-02-06 11:08:40 +0200

    exercise_81 done

--- part3/arrays/exercise_81/Program.cs
+++ part3/arrays/exercise_81/Program.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 
 namespace exercise_81
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // You can test your method here
+        public static void Main(string[] args)
+        {
+            // You can test your method here
 
-    }
+        }
 
-    public static void PrintNeatly(int[] array)
-    {
+        public static void PrintNeatly(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++){
 
-    }
+                if (i < array.Length - 1)
+                {
+
+                    Console.Write(array[i] + ", ");
+                }
+                else
+                {
+                    Console.Write(array[i]);
+                }
+                
+            }
+          Console.WriteLine();
+        }
+    }    
   }
-}
 
