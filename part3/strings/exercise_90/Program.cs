f48b04b0a8bb5901a6242dc2feb79e1ad0479112
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-02-06 12:49:54 +0200

    exercise_90 done

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -7,8 +7,20 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
+        List<string> persons = new List<string>();
+        int oldest = 0;
+        while(true) {
+          string asked = Console.ReadLine();
+          if (asked == "") {
+            break;
+          }
+          string[] parts = asked.Split(",");
+         if (Convert.ToInt32(parts[1]) > oldest) {
+           oldest = Convert.ToInt32(parts[1]);
+        }
+      }
 
-
+     Console.WriteLine("Age of the oldest: " + oldest);
     }
   }
 }
