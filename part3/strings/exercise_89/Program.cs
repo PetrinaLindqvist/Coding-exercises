23644f480466fe6c3e604eb7791dc8e5b32c7cf3
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-02-08 15:46:08 +0200

    exercise_89 done

--- part3/strings/exercise_89/Program.cs
+++ part3/strings/exercise_89/Program.cs
@@ -7,8 +7,20 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
+            while (true)
+            {
+                string asked = Console.ReadLine();
 
+                if (asked == "")
+                {
+                    break;
+                }
+                string[] parts = asked.Split(" ");
 
+                  {
+                   Console.WriteLine(parts[parts.Length-1]);
+                  }
+            }
     }
   }
 }
