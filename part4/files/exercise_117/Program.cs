56c2977ff04f69491acf160d933532b5f7f162c9
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-02-14 13:50:27 +0200

    exercise_117 done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -8,7 +8,12 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
-
+      
+      Console.WriteLine("Which file should have its contents printed?" );
+      string file = Console.ReadLine();
+      
+      string text = File.ReadAllText(file);
+      Console.WriteLine(text);
       
     }
   }
