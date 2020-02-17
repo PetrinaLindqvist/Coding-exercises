14c9145a3ccac7f87dec6e21e0d9a4825d8cb006
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-02-17 17:58:48 +0200

    exercise_119 done

--- part5/revision/exercise_119/Program.cs
+++ part5/revision/exercise_119/Program.cs
@@ -8,7 +8,28 @@ namespace exercise_119
   {
     public static void Main(string[] args)
     {
-
+       Timer timer = new Timer();
+  // Loop until you cancel the loop.
+  // You can cancel with the CTRL + C
+  while (true)
+  {
+    Console.WriteLine(timer);
+    timer.Advance();
+    // Some error proving, we'll talk about this later.
+    // Known as try-catch.
+    try
+    {
+      // Wait 100th of a second. 
+      // Sleep(1000) waits one second, if you want to test at slower pace.
+      System.Threading.Thread.Sleep(10);
+    }
+    // Other half of try-catch pair. 
+    catch (Exception e)
+    {
+      Console.WriteLine("Error happened: +" + e);
     }
   }
 }
+    }
+  }
+
