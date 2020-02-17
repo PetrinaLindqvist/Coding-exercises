007060cf4ebd33858dac9d4c3a147ff23f189461
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-02-17 18:43:52 +0200

    exercise_120 done

--- part5/revision/exercise_120/Program.cs
+++ part5/revision/exercise_120/Program.cs
@@ -7,7 +7,8 @@ namespace exercise_120
     public static void Main(string[] args)
     {
       // You can test your cube here.
-      
+      Cube mycube = new Cube(6);
+      Console.WriteLine(mycube);
     }
   }
 }
