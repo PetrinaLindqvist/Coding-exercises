4845e9bdec09032ad0f3dedc2258a6cb53b2cd20
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-02-18 12:30:21 +0200

    exercise_122 done

--- part5/overloading/exercise_122/Product.cs
+++ part5/overloading/exercise_122/Product.cs
@@ -8,11 +8,24 @@ namespace exercise_122
     private int weight;
 
     // CREATE YOUR CONSTRUCTORS HERE
-
-
-
-
-
+  public Product(string name) 
+    {
+      this.name = name;
+      this.location = "shelf";
+      this.weight = 1;
+    }
+  public Product(string name, string location)
+    {
+      this.name = name;
+      this.location = location;
+      this.weight = 1;
+    }
+  public Product(string name, int weight)
+    {
+      this.name = name;
+      this.location = "warehouse";
+      this.weight = weight;
+    }
     // DO NOT TOUCH THIS
     public override string ToString()
     {
