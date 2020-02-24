3e8a26135351d94bcfa5f43b07ce45ddf42cb264
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-02-24 11:50:40 +0200

    exercise_131 done

--- part5/references/exercise_131/Item.cs
+++ part5/references/exercise_131/Item.cs
@@ -20,9 +20,31 @@ namespace exercise_131
     {
 
       //compare with Item.identifier
-
-      return false;
+       if (this == compared)
+      {
+        return true;
       }
+
+      // if the compared object is null, or
+      // if the type of the compared object is not SimpleDate, the objects are not equal
+      if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+      {
+        return false;
+      }
+
+      // convert the Object type compared object
+      // into an SimpleDate type object called comparedSimpleDate
+      Item comparedItem = (Item)compared;
+
+      // if the values of the object variables are the same, the objects are equal
+      if (this.identifier == comparedItem.identifier) 
+      { 
+        return true;
+      }
+
+      // otherwise the objects are not equal
+      return false;
+     
     }
 
   }
--- part5/references/exercise_131/Program.cs
+++ part5/references/exercise_131/Program.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 
 namespace exercise_131
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<Item> items = new List<Item>();
+        public static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
 
-      // Ask for input as shown in the exercise.
+            // Ask for input as shown in the exercise.
+            while (true)
+            {
+                Console.WriteLine("Identifier? (empty will stop):");
+                string identifier = Console.ReadLine();
+                if (identifier == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Name? (empty will stop):");
+                string name = Console.ReadLine();
 
-      // The end printing is ready, don't touch this
-      Console.WriteLine("==Items==");
-      foreach (Item item in items)
-      {
-        Console.WriteLine(item);
-      }
+                if (name == "")
+                {
+                    break;
+                }
+
+                Item item = new Item(identifier, name);
+                if (items.Contains(item))
+                {
+                    continue;
+                }
+                else
+                {
+                    items.Add(item);
+                }
+            }
+            // The end printing is ready, don't touch this
+            Console.WriteLine("==Items==");
+            foreach (Item item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
 
     }
-
-  }
 }
