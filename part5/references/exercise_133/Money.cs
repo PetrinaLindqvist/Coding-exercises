9f1dafeb2e68372ff06eb80378bd501972101e1c
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-02-24 14:15:05 +0200

    exercise_133 done

--- part5/references/exercise_133/Money.cs
+++ part5/references/exercise_133/Money.cs
@@ -20,25 +20,51 @@ namespace exercise_133
 
     public Money Plus(Money addition)
     {
-      Money newMoney = new Money(/* Do something here*/);
+      Money newMoney = new Money(this.euros, this.cents);/* Do something here*/
       // create a new Money object that has the correct worth
-
-      // return the new Money object
+      newMoney.euros += addition.euros;
+      newMoney.cents += addition.cents;
+     
+      if (newMoney.cents > 99)
+      {
+        newMoney.euros = newMoney.euros + newMoney.cents / 100;
+        newMoney.cents = newMoney.cents % 100;
+      }
       return newMoney;
     }
 
     public Money Minus(Money decreaser)
     {
-      Money newMoney = new Money(/* Do something here*/);
+      Money newMoney = new Money(this.euros, this.cents);
       // create a new Money object that has the correct worth
+      newMoney.euros -= decreaser.euros;
+      newMoney.cents -= decreaser.cents;
 
+      if(newMoney.cents < 0)
       // return the new Money object
+      {
+        newMoney.euros = newMoney.euros - 1;
+        newMoney.cents = newMoney.cents + 100;
+      }
+      if (newMoney.euros < 0 || newMoney.cents < 0)
+      {
+        newMoney.euros = 0;
+        newMoney.cents = 0;
+      }
       return newMoney;
     }
 
     public bool LessThan(Money compared)
     {
       // Do something here
+      if (compared.euros > this.euros)
+      {
+        return true;
+      }
+      if (compared.euros == this.euros && compared.cents > this.cents)
+      {
+        return true;
+      }
       return false;
     }
 
