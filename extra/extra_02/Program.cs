c3b9cc63598025f3340e8ebe082430e9dacb7573
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-10 19:33:17 +0200

    extra 2 done

--- extra/extra_02/Program.cs
+++ extra/extra_02/Program.cs
@@ -7,7 +7,23 @@ namespace extra_02
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("Give three numbers:");
+      int userinput1 = Convert.ToInt32(Console.ReadLine());
+      int userinput2 = Convert.ToInt32(Console.ReadLine());
+      int userinput3 = Convert.ToInt32(Console.ReadLine());
 
+      if (userinput1 > userinput2 && userinput1 > userinput3)
+      {
+        Console.WriteLine("Largest: " + userinput1);
+      }
+      else if (userinput2 > userinput1 && userinput2 > userinput3)
+      {
+        Console.WriteLine("Largest: " + userinput2);
+      }
+      else
+      {
+        Console.WriteLine("Largest: " + userinput3);
+      }
     }
   }
 }
