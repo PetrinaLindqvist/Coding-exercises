8ccf348a81108d90d1faf254a3173485695d75b6
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-10 20:52:09 +0200

    extra 3 not quite yet

--- extra/extra_03/Program.cs
+++ extra/extra_03/Program.cs
@@ -7,7 +7,32 @@ namespace extra_03
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("How many numbers? ");
+      int input = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give " + input + " numbers: ");
+      int times = 0;
+      int count = 0;
+      int sum = 0;
+      while (true)
+      {
+        count = Convert.ToInt32(Console.ReadLine());
+        times++;
+      
+      if (times == input)
+      {
+        break;
+      }
 
+      sum = sum + count;
+      }
+      double average = (double)sum / times;
+      Console.WriteLine("Their sum: " + sum);
+      Console.WriteLine("Their total: " + (count * count));
+      Console.WriteLine("Their average: " + (count + count)/count);
+    
+    
+    
+    
     }
   }
 }
