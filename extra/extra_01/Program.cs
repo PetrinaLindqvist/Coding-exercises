f954eae225654519eb3fd02a9564b2525936bd19
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-10 18:41:56 +0200

    extra 1 done

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -7,7 +7,23 @@ namespace extra_01
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      
+      Console.WriteLine("What do you want to print?");
+      string input = Console.ReadLine();
+      Console.WriteLine("How many times do you want to print?");
+      int times = Convert.ToInt32(Console.ReadLine());
+      int count = 0;
+      while (true)
+      { 
+        Console.WriteLine(input);
+        count++;
+      
+        if (count == times)
+        {
+          break;
+        }
+      }
+    
     }
   }
 }
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace sandbox
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace sandbox {
+    class Program {
+        static void Main (string[] args) {
+            ChristmasTree (10);
+
         }
+        public static void PrintStars (int number) {
+            for (int i = 0; i < number; i++) {
+                Console.Write ("*");
+            }
+            Console.WriteLine ("");
+        }
+
+        public static void PrintSpaces (int number) {
+            for (int i = 0; i < number; i++) {
+                Console.Write (" ");
+            }
+        }
+        public static void PrintRightTriangle (int size) {
+            for (int i = 1; i <= size; i++) {
+                PrintSpaces (size - i);
+                PrintStars (i);
+            }
+        }
+        public static void ChristmasTree (int height) 
+        {
+            for (int i = 1; i <= height; i++) 
+            {
+                PrintSpaces (height - i);
+                PrintStars ((2 * i) - 1);
+            }
+            PrintSpaces (height - 3);
+            PrintStars (3);
+            PrintSpaces (height - 3);
+            PrintStars (3);
+        }
+
     }
+
 }
