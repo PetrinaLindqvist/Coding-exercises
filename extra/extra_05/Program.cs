c90dbdb5bfa13980f1897415a8a84c44844bc401
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-03-12 11:47:42 +0200

    extra 5 done

--- extra/extra_05/Program.cs
+++ extra/extra_05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace extra_05
 {
@@ -7,7 +8,27 @@ namespace extra_05
     public static void Main(string[] args)
     {
       // Add your code here:
+      List<string> list = new List<string>();
+      for (int i = 1; i < 6; i ++)
+      {
+        Console.WriteLine(i);
+      }
+      
+      Console.WriteLine("Give words, 'end' quits:");
+      
+        while (true)
+        {
+        string word = Console.ReadLine();
+        if (word == "end")
+        {
+          break;
+        }
+        list.Add(word);
+        }
+        list.ForEach(Console.WriteLine);
 
+
+       
     }
-  }
+  } 
 }
