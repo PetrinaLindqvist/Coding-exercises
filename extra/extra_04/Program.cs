cde2c94084a6f928d18bd37ad5cc4276c24b1583
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-03-12 10:58:48 +0200

    extra 4 done

--- extra/extra_04/Program.cs
+++ extra/extra_04/Program.cs
@@ -7,7 +7,40 @@ namespace extra_04
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      Console.WriteLine("Give your percent [0 - 100]: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+      if (number >= 0 && number < 50)
+      {
+        Console.WriteLine("Fail");
+      }
+      else if (number >= 50 && number < 60)
+      {
+        Console.WriteLine("Grade: 1");
+      }
+      else if (number >= 60 && number < 70)
+      {
+        Console.WriteLine("Grade: 2");
+      }
+      else if (number >= 70 && number < 80)
+      {
+        Console.WriteLine("Grade: 3");
+      }
+      else if (number >= 80 && number < 90)
+      {
+        Console.WriteLine("Grade: 4");
+      }
+      else if (number >= 90 && number < 101)
+      {
+        Console.WriteLine("Grade: 5");
+      }
+      else if (number >= 100)
+      {
+        Console.WriteLine("Outstanding!");
+      }
+      else 
+      {
+        Console.WriteLine("Impossible");
+      }
     }
   }
 }
