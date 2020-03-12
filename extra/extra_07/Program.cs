5cf9ffd1f9f97e6d8889d96ee5f1f25f9aff2c9c
Petrina Lindqvist SOFTK20P <petrina.lindqvist@centria.fi>
2020-03-12 13:49:59 +0200

    extra 7 not ready

--- extra/extra_07/Program.cs
+++ extra/extra_07/Program.cs
@@ -7,7 +7,20 @@ namespace extra_07
     public static void Main(string[] args)
     {
       // Add your code here:
-
+      Console.WriteLine("Give integers, 'end' quits:");
+      
+      while (true)
+      {
+      int nbr = Convert.ToInt32(Console.ReadLine());
+      string word = Console.ReadLine();
+     
+        if (word == "end")
+        {
+          break; 
+        }
+        Console.WriteLine("Sum: " + (nbr + nbr));
+      }
+    
     }
   }
 }
