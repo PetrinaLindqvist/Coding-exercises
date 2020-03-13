df45e493d541414381266c92c1d5be02f244feae
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-13 10:33:08 +0200

    extra 12 done

---
+++ extra/extra_12/Person.cs
@@ -0,0 +1,26 @@
+namespace extra_12 
+{
+    public class Person 
+    {
+
+        public string name;
+        public int age;
+
+        public Person (string initialName) 
+        {
+            this.name = initialName;
+            this.age = 0;
+        }
+
+        public int GrowOlder(int howMuch)
+        {
+              return this.age = this.age + howMuch;    
+        }       
+   
+
+        public override string ToString () 
+        {
+            return this.name + ", " + "age " + this.age;
+        }
+    }
+}
--- extra/extra_12/Program.cs
+++ extra/extra_12/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 
-namespace extra_12
+namespace extra_12 
 {
-  class Program
+  class Program 
   {
-    public static void Main(string[] args)
+  
+    public static void Main (string[] args) 
     {
       // Add your code here:
-
+      Person mike = new Person("Mike");
+      Console.WriteLine (mike);
+      mike.GrowOlder (5);
+      Console.WriteLine (mike);
     }
   }
 }
