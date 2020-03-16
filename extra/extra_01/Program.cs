e7fd837fb7522a9d08edfda3766e3865b6b7c9f5
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-16 18:26:51 +0200

    extra 1 done

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 
-namespace exercise_01
-{
-    class Program
-    {
-        public static void Main(string[] args)
+namespace extra_01 {
+  class Program {
+    public static void Main (string[] args) {
+      // Add your code here:
+      Console.WriteLine ("What do you want to print?");
+      string input = Console.ReadLine ();
+      Console.WriteLine ("How many times do you want to print?");
+      int times = Convert.ToInt32 (Console.ReadLine ());
+      
+      int count = 0;
+      while (true) 
+      {
+        Console.WriteLine (input);
+        count++;
+
+        if (count == times)
         {
-            // Add your code here:
-            Console.WriteLine("Hello World!");
-
-
+          break;
         }
+
+      }
     }
+  }
 }
