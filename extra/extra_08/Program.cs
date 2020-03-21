a76d189d609e91b3dfef5ff0494bc1b902be5cb3
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-21 09:38:40 +0200

    exercise_01 done

--- extra/extra_08/Program.cs
+++ extra/extra_08/Program.cs
@@ -7,7 +7,54 @@ namespace extra_08
     public static void Main(string[] args)
     {
       // Add your code here:
+      ChristmasTree(4);
 
     }
+      public static void PrintStars(int number)
+      {
+        for (int i = 0; i < number; i++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");
+      
+      }
+      
+      public static void PrintSpaces(int size)
+      {
+        for (int i = 1; i <= size; i++)
+        {
+          PrintSpaces(size-i);
+          PrintStars(i);
+        }
+      
+      
+      }
+      
+      public static void PrintRightTriangle(int size)
+    {
+      int left = size -1;
+      for (int i = 1; i <= size; i++)
+      {
+        PrintSpaces(left);
+        PrintStars(i);
+        left--;
+      }
+      
+    } 
+      public static void ChristmasTree(int height)
+      {
+        for (int i= 1; i<= height; i++)
+        {
+          PrintSpaces(height-i);
+          PrintStars ((2*i)-1);
+        }
+        PrintSpaces(height-2);
+        PrintStars(3);
+        PrintSpaces(height -2);
+        PrintStars(3);
+      
+      }
+    
   }
 }
