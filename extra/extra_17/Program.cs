d82df7b6d63f117fbea3f4b11224eb8117e3c8d0
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-21 15:48:31 +0200

    exercise 17 started

--- extra/extra_17/Program.cs
+++ extra/extra_17/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace extra_17
 {
   class Program
@@ -7,7 +7,41 @@ namespace extra_17
     public static void Main(string[] args)
     {
       // Add your code here:
+      List<String> list = new List<String>();
+       
+        while (true)
+        {
+          Console.WriteLine("Identifying number?");
+          string number = (Console.ReadLine());
+        
+        if (number == "")
+        {
+          break;
+        }
+        
+        Console.WriteLine("Name? (empty will stop):");
+        string name = Console.ReadLine();
 
+        if (name == "")
+        {
+          break;
+        }
+       
+        
+        String identity = new String(number, name);
+        if (list.Contains(identity))
+        {
+          list.Add(identity);
+        }
+        
+      }
+        Console.WriteLine("==Persons==");
+        foreach(String identity in list)
+        {
+          Console.WriteLine(identity);
+        }
+       
+      }
     }
   }
-}
+
