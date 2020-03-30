19ab01e4893411379400c5b744d10ac9fcc2f971
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-30 09:08:59 +0300

    exercise 138 done

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 
-namespace Exercise
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
+namespace Exercise {
+  public class Program {
+    public static void Main (string[] args) {
       // Do something here
+      Dictionary<string, string> nickNames = new Dictionary<string, string> ();
+      nickNames.Add ("matthew", "matt");
+      nickNames.Add ("michael", "mix");
+      nickNames.Add ("arthur", "artie");
 
+      foreach (KeyValuePair<string, string> kvp in nickNames) 
+      {
+        Console.WriteLine ("{0}'s nickname is {1}",
+          kvp.Key, kvp.Value);
       }
+
     }
+
   }
 }
---
+++ project_examples/NewTypesTest/PetTests.cs
@@ -0,0 +1,24 @@
+using System;
+using NUnit.Framework;
+using Pets;
+
+public class PetTests
+{
+  [Test]
+  public void DogTalkToOwnerReturnsWoof()
+  {
+    string expected = "Woof!";
+    string actual = new Dog().TalkToOwner();
+
+    Assert.AreNotEqual(expected, actual);
+  }
+
+  [Test]
+  public void CatTalkToOwnerReturnsMeow()
+  {
+    string expected = "Meow!";
+    string actual = new Cat().TalkToOwner();
+
+    Assert.AreNotEqual(expected, actual);
+  }
+}
