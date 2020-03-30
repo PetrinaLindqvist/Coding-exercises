46655ed22b36b488200229329e471a81e9acaf48
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-30 20:39:20 +0300

    exercise 140 done

--- part8/exercise_140/src/Exercise/Program.cs
+++ part8/exercise_140/src/Exercise/Program.cs
@@ -1,37 +1,58 @@
 using System;
 using System.Collections.Generic;
 
-namespace Exercise
+namespace Exercise 
 {
-  public class Program
+  public class Program 
   {
-    public static void Main(string[] args)
+    public static void Main (string[] args) 
     {
-      Dictionary<string, string> dict = new Dictionary<string, string>();
-      dict.Add("f.e", "for example");
-      dict.Add("etc.", "and so on");
-      dict.Add("i.e", "more precisely");
+      Dictionary<string, string> dict = new Dictionary<string, string> ();
+      dict.Add ("f.e", "for example");
+      dict.Add ("etc.", "and so on");
+      dict.Add ("i.e", "more precisely");
 
-      PrintKeys(dict);
-      Console.WriteLine("---");
-      PrintKeysWhere(dict, "i");
-      Console.WriteLine("---");
-      PrintValuesOfKeysWhere(dict, ".e");
+      PrintKeys (dict);
+      Console.WriteLine ("---");
+      PrintKeysWhere (dict, "i");
+      Console.WriteLine ("---");
+      PrintValuesOfKeysWhere (dict, ".e");
     }
 
-    public static void PrintKeys(Dictionary<string, string> dict)
+    public static void PrintKeys(Dictionary<string, string> dict) 
+    {
+      foreach (KeyValuePair<string, string> kvp in dict)
+      {
+        Console.WriteLine(kvp.Key);  
+      }
+    }
+    public static void PrintKeysWhere(Dictionary<string, string> dict, string text) 
+    {
+       Dictionary<string, string>.KeyCollection keys = dict.Keys;
+
+      foreach (string key in keys) 
+      {
+        if (key.Contains(text)) 
+        {
+          Console.WriteLine(key);
+        }
+      }
+    }
+    public static void PrintValuesOfKeysWhere(Dictionary<string, string> dict, string text) 
     {
 
-    }
-    public static void PrintKeysWhere(Dictionary<string, string> dict, string text)
-    {
+      Dictionary<string, string>.KeyCollection keys = dict.Keys;
+
+      foreach (string key in keys) 
+      {
+        if (key.Contains(text)) 
+        {
+          Console.WriteLine(dict[key]);
+        }
+      }
+      
 
     }
 
-    public static void PrintValuesOfKeysWhere(Dictionary<string, string> dict, string text)
-    {
-
-    }
   }
 }
-
