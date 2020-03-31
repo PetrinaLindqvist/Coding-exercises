8541a7a5f67e9f0e9058738e3881905f36a7faf5
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-03-31 18:09:28 +0300

    exercise 141 started

--- part8/exercise_141/src/Exercise/Program.cs
+++ part8/exercise_141/src/Exercise/Program.cs
@@ -1,31 +1,43 @@
 using System;
 using System.Collections.Generic;
 
-namespace Exercise
+namespace Exercise 
 {
-  public class Program
+  public class Program 
   {
-    public static void Main(string[] args)
+    public static void Main (string[] args) 
     {
-      Dictionary<string, Book> books = new Dictionary<string, Book>();
-      Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
-      Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "....");
-      books.Add(senseAndSensibility.name, senseAndSensibility);
-      books.Add(prideAndPrejudice.name, prideAndPrejudice);
+      Dictionary<string, Book> books = new Dictionary<string, Book> ();
+      Book senseAndSensibility = new Book ("Sense and Sensibility", 1811, "...");
+      Book prideAndPrejudice = new Book ("Pride and Prejudice", 1813, "....");
+      books.Add (senseAndSensibility.name, senseAndSensibility);
+      books.Add (prideAndPrejudice.name, prideAndPrejudice);
 
-      PrintValues(books);
-      Console.WriteLine("-- -- -- --");
-      PrintValueIfNameContains(books, "prejud");
+      PrintValues (books);
+      Console.WriteLine ("-- -- -- --");
+      PrintValueIfNameContains (books, "prejud");
     }
 
-    public static void PrintValues(Dictionary<string, Book> dictionary)
+    public static void PrintValues(Dictionary<string, Book> dictionary) 
     {
+      
+      foreach (KeyValuePair<string, Book> kvp in dictionary)
+      {
+          Console.WriteLine(kvp.Value);
+      }
 
     }
-    public static void PrintValueIfNameContains(Dictionary<string, Book> dictionary, string text)
+    public static void PrintValueIfNameContains (Dictionary<string, Book> dictionary, string text) 
     {
-
+      foreach (KeyValuePair<string, Book> kvp in dictionary)
+     
+      text = text.ToLower();
+      if (kvp.Value.name.Contains(text)) 
+      {
+         Console.WriteLine(kvp.Value); 
+      }
+    
+   
     }
   }
 }
-
