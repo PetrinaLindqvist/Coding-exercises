c59eeb793bee95c76e332d153161cc69e220dcc1
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-03 15:32:05 +0300

    exercise 141 done

--- part8/exercise_141/src/Exercise/Program.cs
+++ part8/exercise_141/src/Exercise/Program.cs
@@ -5,41 +5,41 @@ namespace Exercise
 {
   public class Program 
   {
-    public static void Main (string[] args) 
+    public static void Main(string[] args) 
     {
       Dictionary<string, Book> books = new Dictionary<string, Book> ();
-      Book senseAndSensibility = new Book ("Sense and Sensibility", 1811, "...");
-      Book prideAndPrejudice = new Book ("Pride and Prejudice", 1813, "....");
-      books.Add (senseAndSensibility.name, senseAndSensibility);
-      books.Add (prideAndPrejudice.name, prideAndPrejudice);
+      Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
+      Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "....");
+      books.Add(senseAndSensibility.name, senseAndSensibility);
+      books.Add(prideAndPrejudice.name, prideAndPrejudice);
 
-      PrintValues (books);
-      Console.WriteLine ("-- -- -- --");
-      PrintValueIfNameContains (books, "prejud");
+      PrintValues(books);
+      Console.WriteLine("-- -- -- --");
+      PrintValueIfNameContains(books, "prejud");
     }
 
     public static void PrintValues(Dictionary<string, Book> dictionary) 
     {
-      
-      foreach (KeyValuePair<string, Book> kvp in dictionary)
+
+      foreach (KeyValuePair<string, Book> kvp in dictionary) 
       {
-          Console.WriteLine(kvp.Value);
+        Console.WriteLine(kvp.Value);
       }
 
     }
-    public static void PrintValueIfNameContains (Dictionary<string, Book> dictionary, string text) 
-    {
-      foreach (KeyValuePair<string, Book> kvp in dictionary)
-            {
-                string lowCase = kvp.Value.name.ToLower();
+    public static void PrintValueIfNameContains(Dictionary<string, Book> dictionary, string text)
+     {
+      foreach (KeyValuePair<string, Book> kvp in dictionary) 
+      {
+        string lowCase = kvp.Value.name.ToLower ();
 
-                if (!lowCase.Contains(text))
-                {
-                    continue;
-                }
-                Console.WriteLine(kvp.Value);
-            }
-
+        if (!lowCase.Contains (text)) 
+        {
+          continue;
         }
+        Console.WriteLine (kvp.Value);
+      }
+
+    }
   }
 }
