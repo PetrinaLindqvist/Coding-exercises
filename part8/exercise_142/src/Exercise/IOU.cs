11dc569ff33123b6da34695711dc74b8bbea1896
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-03 18:35:37 +0300

    exercise 142 done

--- part8/exercise_142/src/Exercise/IOU.cs
+++ part8/exercise_142/src/Exercise/IOU.cs
@@ -1,21 +1,50 @@
-
+using System.Collections.Generic;
 
 namespace Exercise
 {
   public class IOU
   {
+    private Dictionary<string, int> dictionary;
     public IOU()
     {
-
+      this.dictionary = new Dictionary<string, int>();
     }
     public void ChangeDebt(string toWhom, int amount)
     {
-
+      if (this.dictionary.ContainsKey(toWhom))
+      {
+        int newAmount = this.dictionary[toWhom] + amount;
+        if (newAmount > 0)
+        {
+          this.dictionary[toWhom] = newAmount;
+        }
+        else
+        {
+          this.dictionary[toWhom] = 0;
+        }
+      }
+      else if (amount > 0)
+      {
+        this.dictionary[toWhom] = amount;
+      }
+      else
+      {
+        this.dictionary[toWhom] = 0;
+      }
+      
     }
 
     public int HowMuchDoIOweTo(string toWhom)
     {
-      return 0;
+      if (this.dictionary.ContainsKey(toWhom))
+      {
+        return this.dictionary[toWhom];
+      }
+      else
+      {
+        return 0;
+      }
     }
+    
   }
 }
--- part8/exercise_142/src/Exercise/Program.cs
+++ part8/exercise_142/src/Exercise/Program.cs
@@ -1,25 +1,29 @@
 using System;
 using System.Collections.Generic;
 
-namespace Exercise
+namespace Exercise 
 {
   public class Program
-  {
-    public static void Main(string[] args)
+   {
+    public static void Main(string[] args) 
     {
       IOU mattsIOU = new IOU();
+      mattsIOU.ChangeDebt("Arthur", -10);
+
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo ("Arthur"));
+
       mattsIOU.ChangeDebt("Arthur", 51);
       mattsIOU.ChangeDebt("Arthur", 30);
 
-      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo ("Arthur"));
 
       mattsIOU.ChangeDebt("Arthur", -30);
 
-      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo ("Arthur"));
 
       mattsIOU.ChangeDebt("Arthur", -80);
 
-      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo ("Arthur"));
     }
   }
 }
