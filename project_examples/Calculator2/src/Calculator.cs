3a470b4680b7e38a67fd660e75b746a7236894eb
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-05 17:20:30 +0300

    Calculator2 not ready

---
+++ project_examples/Calculator2/src/Calculator.cs
@@ -0,0 +1,20 @@
+public class Calculator
+{
+
+  public int value { get; private set; }
+
+  public Calculator()
+  {
+    this.value = 0;
+  }
+
+  public void Sum(int number)
+  {
+    this.value = this.value + number;
+  }
+
+  public void Substract(int number)
+  {
+    this.value = this.value - number;
+  }
+}
