d7fddcda7db29bdad1fe01d231a0ebcaa6f19cf0
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-05 20:03:29 +0300

    exercise 143 done

--- part8/exercise_143/src/Exercise/SimpleDate.cs
+++ part8/exercise_143/src/Exercise/SimpleDate.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Exercise
 {
   public class SimpleDate
@@ -39,13 +40,33 @@ namespace Exercise
     public override bool Equals(object compared)
     {
       // DO SOMETHING HERE
-      return false;
-    }
+       // if the variables are located in the same position, they are equal
+      if (this == compared)
+      {
+        return true;
+      }
 
+      // if the compared object is null or not of type, the objects are not equal
+      if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+      {
+        return false;
+      }
+      else
+      {
+      // convert the object to a this class
+      SimpleDate comparedDate = (SimpleDate)compared;
+
+      // if the values of the object variables are equal, the objects are, too
+        return this.day == comparedDate.day && this.month == comparedDate.month && this.year == comparedDate.year;
+      }
+    }
     public override int GetHashCode()
     {
       // DO SOMETHING HERE
-      return -1;
+      {
+        return HashCode.Combine (this.day, this.month, this.year);
+      }
+      
     }
   }
 }
