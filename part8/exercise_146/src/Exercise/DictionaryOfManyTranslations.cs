519100995fe24e2035e469472a8fc837484a5ba1
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-07 14:47:39 +0300

    exercise 146 done

--- part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
+++ part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
@@ -1,23 +1,40 @@
+using System;
 using System.Collections.Generic;
 
 namespace Exercise
 {
   public class DictionaryOfManyTranslations
   {
-
+     private Dictionary<string, List<string>> translations;
+     
+    public DictionaryOfManyTranslations()
+    {    
+      this.translations = new Dictionary<string, List<string>>();
+    }
     public void Add(string word, string translation)
     {
-
+      if (!this.translations.ContainsKey(word)) 
+      {
+        this.translations.Add(word, new List<string>());
+      }
+      this.translations[word].Add(translation);
     }
 
     public List<string> Translate(string word)
     {
-      return new List<string>();
+      if (!this.translations.ContainsKey(word))
+      {
+      this.translations.Add(word, new List<string>());
+      }
+      return this.translations[word];
     }
-
     public void Remove(string word)
     {
-
+       if (this.translations.ContainsKey(word)) 
+      {
+        this.translations.Remove(word);
+      }
+      
     }
   }
 }
--- part8/exercise_146/src/Exercise/Program.cs
+++ part8/exercise_146/src/Exercise/Program.cs
@@ -1,13 +1,35 @@
 using System;
 using System.Collections.Generic;
 
-namespace Exercise
+namespace Exercise 
 {
-  public class Program
+  public class Program 
   {
-    public static void Main(string[] args)
+    public static void Main(string[] args) 
     {
+      DictionaryOfManyTranslations dictionary = new DictionaryOfManyTranslations();
+      dictionary.Add("lie", "maata");
+      dictionary.Add("lie", "valehdella");
 
+      dictionary.Add("bow", "jousi");
+      dictionary.Add("bow", "kumartaa");
+
+      foreach (string translation in dictionary.Translate ("bow")) 
+      {
+        Console.WriteLine (translation);
+      }
+      Console.WriteLine();
+
+      foreach (string translation in dictionary.Translate ("lie"))
+      {
+        Console.WriteLine (translation);
+      }
+
+      dictionary.Remove("bow");
+      foreach (string translation in dictionary.Translate ("bow"))
+      {
+        Console.WriteLine (translation);
+      }
     }
   }
 }
