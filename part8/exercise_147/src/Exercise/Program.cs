cfa21d29142a37bd8b00a4b28e9fa350b5ffbc9f
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-07 16:07:01 +0300

    exercise 147 done

--- part8/exercise_147/src/Exercise/Program.cs
+++ part8/exercise_147/src/Exercise/Program.cs
@@ -1,12 +1,48 @@
 using System;
 
-namespace Exercise
+namespace Exercise 
 {
-  public class Program
+  public class Program 
   {
-    public static void Main(string[] args)
+    public static void Main(string[] args) 
     {
+      StorageFacility facility = new StorageFacility();
+      facility.Add("a14", "ice skates");
+      facility.Add("a14", "ice hockey stick");
+      facility.Add("a14", "ice skates");
 
+      facility.Add("f156", "rollerblades");
+      facility.Add("f156", "rollerblades");
+
+      facility.Add("g63", "six");
+      facility.Add("g63", "pi");
+
+      foreach (string unit in facility.StorageUnits()) 
+      {
+        Console.WriteLine(unit);
+      }
+
+      foreach (string item in facility.Contents("a14")) 
+      {
+        Console.WriteLine(item);
+      }
+
+      foreach (string item in facility.Contents ("f156")) {
+        Console.WriteLine (item);
+      }
+      facility.Remove("f156", "rollerblades");
+
+      foreach (string item in facility.Contents("f156")) 
+      {
+        Console.WriteLine(item);
+      }
+
+      facility.Remove("f156", "rollerblades");
+
+      foreach (string unit in facility.StorageUnits()) 
+      {
+        Console.WriteLine(unit);
+      }
     }
   }
 }
--- part8/exercise_147/src/Exercise/StorageFacility.cs
+++ part8/exercise_147/src/Exercise/StorageFacility.cs
@@ -5,24 +5,48 @@ namespace Exercise
 {
   public class StorageFacility
   {
-
+    private Dictionary<string, List<string>> storages;
+    
+    public StorageFacility()
+    {
+      this.storages = new Dictionary<string, List<string>>();
+    }
     public void Add(string unit, string item)
     {
-
+       if (!this.storages.ContainsKey(unit)) 
+      {
+        this.storages.Add(unit, new List<string>());
+      }
+      this.storages[unit].Add(item);
     }
 
     public List<string> Contents(string storageUnit)
     {
-      return new List<string>();
+      if (!this.storages.ContainsKey(storageUnit))
+      {
+       this.storages.Add(storageUnit, new List<string>());
+      }
+      return this.storages[storageUnit];
     }
 
     public void Remove(string storageUnit, string item)
     {
-
+      this.storages[storageUnit].Remove(item);
+      if (this.storages[storageUnit].Count == 0) 
+      {
+        this.storages.Remove(storageUnit);
+      }
     }
 
     public List<string> StorageUnits()
     {
-      return new List<string>();
+      List<string> listStorages = new List<string>();
+      Dictionary<string, List <string>>.KeyCollection key = this.storages.Keys;
+      foreach (string storages in key)
+      {
+        listStorages.Add(storages);
+      }
+      return listStorages;
     }
   }
+}
