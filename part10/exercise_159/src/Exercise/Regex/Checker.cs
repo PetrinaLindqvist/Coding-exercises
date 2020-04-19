949805fe04da7b0c484ece90391e317d4a095735
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-19 21:10:31 +0300

    exercise 159 done

--- part10/exercise_159/src/Exercise/Regex/Checker.cs
+++ part10/exercise_159/src/Exercise/Regex/Checker.cs
@@ -3,18 +3,37 @@ namespace Exercise
   using System.Text.RegularExpressions;
   public class Checker
   {
+    
     public bool DayOfWeek(string str)
     {
+      Regex regex = new Regex("^(mon|tue|wed|thu|fri|sat|sun)$");
+      
+      if (regex.IsMatch(str))
+      {
+        return true;
+      }
       return false;
     }
 
     public bool AllVowels(string str)
     {
+     Regex regex = new Regex("^(a|e|i|o|u)*$");
+      
+      if (regex.IsMatch(str))
+      {
+        return true;
+      } 
       return false;
     }
 
     public bool TimeOfDay(string str)
     {
+      Regex regex = new Regex("^(([01][0-9])|([2][0-3])):[0-5][0-9]:[0-5][0-9]$");
+      
+      if (regex.IsMatch(str))
+      {
+        return true;
+      } 
       return false;
     }
   }
