d4bf8fb3142ba68391b08e9bdad3df795a46d358
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-30 16:28:05 +0300

    exercise 170 done

--- part12/exercise_170/src/Exercise/Program.cs
+++ part12/exercise_170/src/Exercise/Program.cs
@@ -6,12 +6,13 @@ namespace Exercise
   {
     public static void Main(string[] args)
     {
-
-      StreamWriter writer = new StreamWriter("file.txt");
+      
+      using (StreamWriter writer = new StreamWriter("file.txt"))
+      {
       writer.WriteLine("Hello file!"); //writes the string "Hello file!" and line change to the file
       writer.WriteLine("More text");
       writer.Write("And a little extra"); // writes the string "And a little extra" to the file without a line change
-      writer.Close(); // closes the file
+      }
     }
   }
 }
