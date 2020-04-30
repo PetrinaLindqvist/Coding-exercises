9dd360c195a6b27a6c94dfccc387dc45bae66aaa
PetrinaLindqvist <60008215+PetrinaLindqvist@users.noreply.github.com>
2020-04-30 14:00:40 +0300

    exercise 167 done

--- part12/exercise_167/src/Exercise/Program.cs
+++ part12/exercise_167/src/Exercise/Program.cs
@@ -1,7 +1,10 @@
 namespace Exercise
 {
   // using something...
-
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
   public class Program
   {
     public static void Main(string[] args)
